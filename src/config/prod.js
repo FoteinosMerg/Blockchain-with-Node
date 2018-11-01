3fce6dcf30628a4f9e3f05e899f3905a669ca788
FoteinosMerg <foteinosmerg@gmail.com>
2018-11-01 17:40:33 +0100

    Initial commit

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+"use strict";
+module.exports = {
+  HTTP_PORT: process.env.HTTP_PORT,
+  P2P_PORT: process.env.P2P_PORT,
+  PEERS: process.env.PEERS,
+  DIFFICULTY: process.env.DIFFICULTY,
+  MINE_RATE: process.env.MINE_RATE
+};
