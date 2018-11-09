80850d9ffe515f5b6e8b78daf8fd2e562ca5e6c8
FoteinosMerg <foteinosmerg@gmail.com>
2018-11-09 14:46:18 +0100

    add comments

--- src/blockchain/tests/block.test.js
+++ src/blockchain/tests/block.test.js
@@ -4,8 +4,11 @@ const Block = require("../block");
 const sha256 = require("crypto-js/sha256");
 
 describe("tests hash function", () => {
+  // Expected hash
   const hash =
     "afb590f7d47d9403d20c1061c76cd21e157f392614e407231bff35f2b68bcaeb";
+
+  // Parameters
   const index = 99;
   const nonce = 666;
   const previousHash = "-some-random-32-bit-string-here-";
